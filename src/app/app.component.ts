beb85a7ee0a359928ebb561c398bc082efb959ec
ayushhagarwal <ayushgrwl365@gmail.com>
2026-02-20 15:21:31 +0530

    privacy updates and ux tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,24 @@ import { CommonModule } from '@angular/common';
 import { filter } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, RouterLink],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'CourtClash';
-  showMainContent = true;
+    title = 'CourtClash';
+    showMainContent = true;
 
-  constructor(private router: Router) {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: any) => {
-      this.showMainContent = event.url === '/';
-    });
-  }
+    constructor(private router: Router) {
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd)
+        ).subscribe((event: any) => {
+            this.showMainContent = event.url === '/';
+        });
+    }
 
-  isRouteActive(): boolean {
-    return !this.showMainContent;
-  }
+    isRouteActive(): boolean {
+        return !this.showMainContent;
+    }
 }
